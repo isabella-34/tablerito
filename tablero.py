b54e5253d386f7fc61db44a723a2a088f4b84508
isabella-34 <isamolial34@gmail.com>
2026-04-16 10:03:52 -0500

    Update tablero.py

--- tablero.py
+++ tablero.py
@@ -8,7 +8,7 @@ with st.sidebar:
     
     # Canvas dimensions (moved to the top)
     st. subheader ("Dimensiones del Tablero")
-    canvas_width • st.slider("Ancho del tablero", 300, 700, 500, 50)
+    canvas_width = st.slider("Ancho del tablero", 300, 700, 500, 50)
     canvas _height = st.slider("Alto del tablero", 200, 600, 300, 50)
     
     # Drawing mode selector
