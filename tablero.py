35a591fbdf8003c6508780d91ae5de18d8c3822c
isabella-34 <isamolial34@gmail.com>
2026-04-16 10:13:09 -0500

    Update tablero.py

--- tablero.py
+++ tablero.py
@@ -28,7 +28,7 @@ with st.sidebar:
 
 # Create a canvas component with dynamic key
 canvas_result = st_canvas(
-    fill_color="rgba(255, 165, 0, 8.3)", 
+    fill_color="rgba(255, 165, 0, 0.3)", 
     stroke_width-stroke_width, 
     stroke_color=stroke_color, 
     background_color=bg_color, 
